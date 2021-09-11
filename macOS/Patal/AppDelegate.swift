21ebee25b8742edcb7b5448e66a3ffb22e22e7fb
YoungTaek <soomtong@gmail.com>
2021-09-11 17:56:11 +0900

    macos: add notification push requested step

--- macOS/Patal/AppDelegate.swift
+++ macOS/Patal/AppDelegate.swift
@@ -6,15 +6,44 @@
 //
 
 import Cocoa
+import UserNotifications
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet var window: NSWindow!
 
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+
+        let center = UNUserNotificationCenter.current()
+        
+//        center.requestAuthorization(options: [.alert, .badge, .sound], completionHandler: { (granted: Bool, error: Error?) -> Void in
+//            if granted {
+//                print("Yes")
+//            } else {
+//                print("No")
+//            }
+//        })
+        
+//        center.requestAuthorization(options: [.alert, .badge], completionHandler: {
+//            (granted: Bool, error: Error?) -> Void in
+//                if granted {
+//                    //
+//                } else {
+//                    //
+//                }
+//            })
+        
+        center.requestAuthorization(options: [.alert, .badge]) { (granted, error) in
+            print("requested notification")
+            
+            if granted {
+                print("notification granted")
+            } else {
+                print("notification refused")
+            }
+        }
+
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
