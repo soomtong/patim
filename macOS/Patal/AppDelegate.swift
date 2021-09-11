679909cc0695dffccedc796f6965dcdec20d8dba
YoungTaek <soomtong@gmail.com>
2021-09-11 20:18:04 +0900

    macos: add notification into fore ground with sound

--- macOS/Patal/AppDelegate.swift
+++ macOS/Patal/AppDelegate.swift
@@ -14,8 +14,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet var window: NSWindow!
     
     let notificationCenter = UNUserNotificationCenter.current()
-
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+//        let notificationCenter = UNUserNotificationCenter.current()
+        UNUserNotificationCenter.current().delegate = self
 
         notificationCenter.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
             print("requested notification")
@@ -35,13 +37,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         notificatonContent.categoryIdentifier = "alarm"
         notificatonContent.sound = UNNotificationSound.default
         
-//        let trigger = UNNotificationTrigger(false)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
  
-        let request1 = UNNotificationRequest(identifier: UUID().uuidString, content: notificatonContent, trigger: nil)
-        notificationCenter.add(request1)
+        let request1 = UNNotificationRequest(identifier: UUID().uuidString, content: notificatonContent, trigger: trigger)
+        notificationCenter.add(request1) { (error) in
+            if error != nil {
+                print(error?.localizedDescription as Any)
+            }
+        }
         
-        let request2 = UNNotificationRequest(identifier: UUID().uuidString, content: notificatonContent, trigger: nil)
-        notificationCenter.add(request2)
+//        let request2 = UNNotificationRequest(identifier: UUID().uuidString, content: notificatonContent, trigger: trigger)
+//        notificationCenter.add(request2)
         
         
     }
@@ -49,7 +55,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        return completionHandler([.sound])
+    }
+}
