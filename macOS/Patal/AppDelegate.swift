b6c9fe4428185afdafad85baf8b13692aff6cdc2
YoungTaek <soomtong@gmail.com>
2021-09-11 12:11:13 +0900

    init: add macos app template for macos input method

---
+++ macOS/Patal/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Patal
+//
+//  Created by EarthShaker on 2021/09/11.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
