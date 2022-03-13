b535f9b217e888ec1891db550456184dab62d025
YoungTaek <soomtong@gmail.com>
2022-03-13 21:44:26 +0900

    macos: add dummy input controller

---
+++ macOS/Patal/InputController.swift
@@ -0,0 +1,17 @@
+//
+//  InputController.swift
+//  Patal
+//
+//  Created by TemplarAssasin on 2022/03/13.
+//
+
+import Foundation
+import InputMethodKit
+
+@objc(PatInputController)
+internal class InputController: IMKInputController {
+    override func handle(_ event: NSEvent, client sender: Any) -> Bool {
+        NSLog("hello patal input method: \(event)")
+        return false
+    }
+}
