91a4ee7f6e2ce8a038cdbfc7a2c85bd6a276b034
YoungTaek <soomtong@gmail.com>
2024-01-07 01:57:27 +0900

    macos: basic char output

--- macOS/Patal/InputController.swift
+++ macOS/Patal/InputController.swift
@@ -10,8 +10,23 @@ import InputMethodKit
 
 @objc(PatInputController)
 internal class InputController: IMKInputController {
-    override func handle(_ event: NSEvent, client sender: Any) -> Bool {
-        NSLog("hello patal input method: \(event)")
-        return false
+    override func inputText(_ string: String!, client sender: Any!) -> Bool {
+        NSLog(string)
+        guard let client = sender as? IMKTextInput else {
+            return false
+        }
+
+        let defaultChar = "ㅎ하한글?횂!";
+        client.insertText(defaultChar, replacementRange: NSRange(location: NSNotFound, length: NSNotFound))
+        return true
     }
+
+//    override func inputText(_ string: String!, key keyCode: Int, modifiers flags: Int, client sender: Any!) -> Bool {
+//        super.inputText(string, key: keyCode, modifiers: flags, client: sender)
+//    }
+
+//    override func handle(_ event: NSEvent, client sender: Any) -> Bool {
+//        NSLog("hello patal input method: \(event)")
+//        return false
+//    }
 }
