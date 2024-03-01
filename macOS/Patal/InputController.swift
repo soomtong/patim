9fd68e19765abc614a8f7eedc200928b250456e3
YoungTaek <soomtong@gmail.com>
2024-03-01 18:05:43 +0900

    macos: change inputText handler that can get ext keys

--- macOS/Patal/InputController.swift
+++ macOS/Patal/InputController.swift
@@ -4,6 +4,7 @@
 //
 //  Created by TemplarAssassin on 2022/03/13.
 //
+
 import os.log
 
 import Foundation
@@ -11,6 +12,8 @@ import InputMethodKit
 
 @objc(PatInputController)
 internal class InputController: IMKInputController {
+/*
+    // 특수 키 - ESC ENTER SHIFT 등의 입력을 알 수 없음
     override func inputText(_ string: String!, client sender: Any!) -> Bool {
         os_log("입력된 문자: \(string)")
         guard let client = sender as? IMKTextInput else {
@@ -21,10 +24,18 @@ internal class InputController: IMKInputController {
         client.insertText(defaultChar, replacementRange: NSRange(location: NSNotFound, length: NSNotFound))
         return true
     }
+*/
 
-//    override func inputText(_ string: String!, key keyCode: Int, modifiers flags: Int, client sender: Any!) -> Bool {
-//        super.inputText(string, key: keyCode, modifiers: flags, client: sender)
-//    }
+    override func inputText(_ string: String!, key keyCode: Int, modifiers flags: Int, client sender: Any!) -> Bool {
+        os_log("입력된 문자: \(string), \(keyCode), \(flags)")
+        guard let client = sender as? IMKTextInput else {
+            return false
+        }
+
+        let defaultChar = "ㅎ하한글?횂!";
+        client.insertText(defaultChar, replacementRange: NSRange(location: NSNotFound, length: NSNotFound))
+        return true
+    }
 
 //    override func handle(_ event: NSEvent, client sender: Any) -> Bool {
 //        NSLog("hello patal input method: \(event)")
