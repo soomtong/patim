478d84cc770af98d3f71a2fccc83edcdeb20ff28
YoungTaek <soomtong@gmail.com>
2024-11-10 02:58:48 +0900

    macos: tidy

--- macOS/Patal/InputController.swift
+++ macOS/Patal/InputController.swift
@@ -12,29 +12,32 @@ import InputMethodKit
 internal class InputController: IMKInputController {
     let logger = CustomLogger(category: "InputController")
 
-    //let layoutSwitcher = LayoutSwitcher()
-    let key = InputTextKey(string: "", keyCode: 0, flags: 0)
+    // let layoutSwitcher = LayoutSwitcher()
+    let processor = HangulProcessor(layout: "InputmethodHan3ShinPCS")
 
     override func inputText(
         _ string: String!, key keyCode: Int, modifiers flags: Int, client sender: Any!
     ) -> Bool {
         // 재사용하면 성능이 오를까? 클래스 생성을 한 번만 하기로
-        key.bind(string: string, keyCode: keyCode, flags: flags)
+        processor.setKey(string: string, keyCode: keyCode, flags: flags)
 
-        /* esc 키를 눌러 라틴 자판으로 변경하는 기능은 karabiner 세팅을 사용하기로 한다. 이 기능은 보류; */
-        //if key.isEscaped() {
-        //    layoutSwitcher.changeLayout()
-        //    return false
-        //}
+        // esc 키를 눌러 라틴 자판으로 변경하는 기능은 karabiner 세팅을 사용하기로 한다.
+        // 이 기능은 보류.
+        if processor.isEscaped() {
+            // layoutSwitcher.changeLayout()
+            return false
+        }
 
         guard let client = sender as? IMKTextInput else {
             return false
         }
 
-        guard let char = key.getComposedChar() else {
+        guard let char = processor.getComposedChar() else {
             return false
         }
 
+        // 조합중인 글자를 처리하기 위해, 백스페이스를 처리하기 위해 커서를 움직여야 함.
+        // 이 조합 조건들은 모두 테스트 케이스를 먼저 디자인할 것
         client.insertText(char, replacementRange: NSRange(location: NSNotFound, length: NSNotFound))
 
         return true
--- macOS/Patal/KeyProcessor.swift
+++ macOS/Patal/KeyProcessor.swift
@@ -13,7 +13,14 @@ struct ComposedChar {
     var flags: Int
 }
 
-class InputTextKey {
+// 키코드 목록 https://eastmanreference.com/complete-list-of-applescript-key-codes
+let CMD_FLAG = 1_048_576
+let CTRL_FLAG = 262144
+let OPT_FLAG = 524288
+let SHIFT_FLAG = 131072
+let ESC_KEYCODE = 53
+
+class HangulProcessor {
     let logger = CustomLogger(category: "InputTextKey")
 
     var string: String
@@ -21,27 +28,22 @@ class InputTextKey {
     var flags: Int
 
     var buffer: [ComposedChar]
+    var preedit: [unichar]
+    var commit: [unichar]
+    var layout: Han3ShinPcsLayout
 
-    // 키코드 목록 https://eastmanreference.com/complete-list-of-applescript-key-codes
-    private let cmdFlags = 1_048_576
-    private let ctrlFlags = 262144
-    private let optFlags = 524288
-    private let shiftFlags = 131072
-
-    private let escKeyCode = 53
-    private let layout: Han3ShinPcsLayout
-
-    init(string: String, keyCode: Int, flags: Int) {
-        logger.debug("입력키 처리 클래스 초기화")
-        self.string = string
-        self.keyCode = keyCode
-        self.flags = flags
-
+    init(layout: String) {
+        logger.debug("입력키 처리 클래스 초기화: \(layout)")
+        self.string = ""
+        self.keyCode = 0
+        self.flags = 0
         self.buffer = []
+        self.preedit = []
+        self.commit = []
         self.layout = Han3ShinPcsLayout()
     }
 
-    func bind(string: String, keyCode: Int, flags: Int) {
+    func setKey(string: String, keyCode: Int, flags: Int) {
         // 특수 키 - ESC ENTER SHIFT 등의 입력을 위해 keyCode 를 받아야 함
         logger.debug("입력된 문자: \(String(describing: string)), \(keyCode), \(flags)")
 
@@ -51,26 +53,17 @@ class InputTextKey {
 
         let char = ComposedChar(char: self.string, keyCode: self.keyCode, flags: self.flags)
         self.buffer.append(char)
+        logger.debug("버퍼: \(self.buffer.count) \(char.char)")
     }
 
     func isEscaped() -> Bool {
-        if self.keyCode == self.escKeyCode && self.flags == 0 {
-            // test 용도
-            self.buffer.removeAll()
+        if self.keyCode == ESC_KEYCODE && self.flags == 0 {
             return true
         }
         return false
     }
 
     func getComposedChar() -> String? {
-        // 입력된 문자열을 작은 버퍼에 담고 조합되는 글자를 반환한다.
-        // return Optional("ㅎ하한글?횂!")
-
-        // buffer 의 글자를 확인
-        // let keyCodeString = String(self.buffer.last?.keyCode ?? 0)
-        // return keyCodeString
-
-        // keyCode 대신 string 을 직접 사용해 보자.
         // 초성이 들어오면 조합 시작
         if let char = self.buffer.first {
             let composed = self.layout.pickChosung(char: char.char)
