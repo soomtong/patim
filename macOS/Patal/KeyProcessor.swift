36fc2d87d9c320541b6bf765d6a71d4a4b0a7224
YoungTaek <soomtong@gmail.com>
2024-11-23 20:51:04 +0900

    macos: composition test 가

--- macOS/Patal/KeyProcessor.swift
+++ macOS/Patal/KeyProcessor.swift
@@ -30,13 +30,13 @@ let ESC_KEYCODE = 53
 class HangulProcessor {
     let logger = CustomLogger(category: "InputTextKey")
 
-    var string: String
+    var rawChar: String
     var keyCode: Int
     var flags: Int
 
     var buffer: ComposedChar
     var preedit: [unichar]
-    var commit: [unichar]
+    var commit: String
 
     private var state: ComposeState = .none
 
@@ -44,13 +44,13 @@ class HangulProcessor {
 
     init(layout: String) {
         logger.debug("입력키 처리 클래스 초기화: \(layout)")
-        self.string = ""
+        self.rawChar = ""
         self.keyCode = 0
         self.flags = 0
         
         self.buffer = ComposedChar(char: "", keyCode: 0, flags: 0)
         self.preedit = []
-        self.commit = []
+        self.commit = ""
     }
 
     deinit {
@@ -60,11 +60,11 @@ class HangulProcessor {
     func setKey(string: String, keyCode: Int, flags: Int) {
         logger.debug("입력된 문자: \(String(describing: string)), \(keyCode), \(flags)")
 
-        self.string = string
+        self.rawChar = string
         self.keyCode = keyCode
         self.flags = flags
 
-        self.buffer = ComposedChar(char: self.string, keyCode: self.keyCode, flags: self.flags)
+        self.buffer = ComposedChar(char: self.rawChar, keyCode: self.keyCode, flags: self.flags)
         logger.debug("버퍼: \(self.buffer.char)")
     }
 
@@ -75,6 +75,7 @@ class HangulProcessor {
             self.preedit.append(initialConsonan)
             self.state = .composing
             let char = String(utf16CodeUnits: [initialConsonan], count: 1)
+             logger.debug("초성 테스트 결과!: \(char), 조합 중: \(self.preedit)")
             return (self.state, char)
         }
         
@@ -82,15 +83,35 @@ class HangulProcessor {
             self.preedit.append(initialVowel)
             self.state = .composing
             let char = String(utf16CodeUnits: [initialVowel], count: 1)
+            logger.debug("중성 테스트 결과!: \(char), 조합 중: \(self.preedit)")
             return (self.state, char)
         }
-
-        // todo: 각각의 낱자를 조합하는 과정이 필요함
-        // self.commit = self.preedit
-        //            return "\(initialConsonant)\(vowelComposed)"
-        // let utf16CodeUnits: [unichar] = [chosung.rawValue]
-        // return (chosung, String(utf16CodeUnits: utf16CodeUnits, count: utf16CodeUnits.count))
-        logger.debug("조합 대상: \(self.preedit)")
-        return (self.state, "조합 중!")
+        
+        self.composePreedit()
+        
+        logger.debug("조합 결과: \(self.commit)")
+        return (self.state, self.commit)
+    }
+    
+    func composePreedit() {
+        if self.preedit.count > 0 {
+                // make unicode hangul char by unichar code point
+            self.commit = self.convertFromCodepoint()
+            logger.debug("조합 된 글자: \(self.commit)")
+            if self.state == .committed {
+                self.preedit = []
+            }
+        }
+        logger.debug("조합 결과: \(self.commit)")
+    }
+    
+    func convertFromCodepoint() -> String {
+        var result: [unichar] = []
+        
+        for codePoint in self.preedit {
+            result.append(codePoint)
+        }
+        
+        return String(utf16CodeUnits: result, count: result.count)
     }
 }
--- macOS/PatalTests/KeyProcessorTests.swift
+++ macOS/PatalTests/KeyProcessorTests.swift
@@ -16,7 +16,7 @@ struct KeyProcessorTests {
         let processor = HangulProcessor(layout: "InputmethodHan3ShinPCS")
 
         processor.setKey(string: "i", keyCode: 34, flags: 0)
-        #expect(processor.string == "i")
+        #expect(processor.rawChar == "i")
         #expect(processor.keyCode == 34)
         #expect(processor.flags == 0)
     }
@@ -28,13 +28,22 @@ struct KeyProcessorTests {
         processor.setKey(string: "k", keyCode: 40, flags: 0)
         if let (state1, test1) = processor.getComposedChar() {
             #expect(state1 == .composing)
-            #expect(test1 == "ㄱ")
+            #expect(test1.hash == "ᄀ".hash)
+            #expect(processor.preedit.count == 1)
         }
 
         processor.setKey(string: "f", keyCode: 3, flags: 0)
         if let (state2, test2) = processor.getComposedChar() {
             #expect(state2 == .composing)
-            #expect(test2 == "가")
+            #expect(test2.hash == "ᅡ".hash)
+            #expect(processor.preedit.count == 2)
+        }
+        
+        processor.setKey(string: "", keyCode: 32, flags: 0)
+        if let (state3, test3) = processor.getComposedChar() {
+            #expect(state3 == .committed)
+            #expect(test3.hash == "가".hash)
+            #expect(processor.preedit.isEmpty)
         }
     }
 }
