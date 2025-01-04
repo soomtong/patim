437d5b135ff61989fc2a1996384ae9f4cb7ffcb6
YoungTaek <soomtong@gmail.com>
2025-01-04 10:18:39 +0900

    macos: bump version 1.0.2 up

--- script/distribute.sh
+++ script/distribute.sh
@@ -4,7 +4,7 @@ set -e
 # "Apple Development: soomtong@gmail.com (4XWP9KHTYS)"
 
 TITLE="팥알 입력기"
-VERSION="1.0.1"
+VERSION="1.0.2"
 APPID="com.soomtong.inputmethod.Patal"
 BUILD="PatalInputMethod"
 PKG="$BUILD.pkg"
