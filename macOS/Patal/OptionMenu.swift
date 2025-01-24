7c552c5840e63f259445d9ac7aaf6e48da085da9
YoungTaek <soomtong@gmail.com>
2025-01-25 08:25:00 +0900

    macos: update menu items in ordered

--- macOS/Patal/OptionMenu.swift
+++ macOS/Patal/OptionMenu.swift
@@ -18,7 +18,8 @@ class OptionMenu {
         menu = NSMenu(title: "PatalLayoutMenu")
         logger.debug("자판의 모든 옵션: \(layout.availableTraits), 현재 선택된 옵션: \(layout.traits)")
 
-        for trait in layout.availableTraits {
+        let sortedTraits = Array(layout.availableTraits).sorted { $0.rawValue < $1.rawValue }
+        for trait in sortedTraits {
             let item = NSMenuItem()
             item.title = trait.rawValue
             item.tag = trait.hashValue
@@ -32,7 +33,5 @@ class OptionMenu {
 
             menu.addItem(item)
         }
-
-        menu.autoenablesItems = true
     }
 }
